6d27db55285d6b77ff2b7f8cb1f926a62b963b96
Marcos <marferp2@inf.upv.es>
2023-12-03 23:03:45 +0100

    feat: init

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	// Define a route for the home page
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title":   "Identity Details",
+			"name":    "Marcos Fernandez",
+			"email":   "marferp2@inf.upv.es",
+			"address": "Valencia (Spain🇪🇸)",
+			"github":  "github.com/markettes",
+		})
+	})
+
+	// Load HTML templates from the "templates" directory
+	router.LoadHTMLGlob("templates/*.tmpl")
+
+	// Start the server on port 8080
+	router.Run(":8080")
+}
